9efda6bde321cce0583ce07be828cde499988667
Curtis Lawhorn <curtis@lawhorns.com>
2024-11-29 19:59:56 -0600

    Added secret

--- src/weather_forecast.py
+++ src/weather_forecast.py
@@ -1,17 +1,44 @@
-#import os
 import requests
+import json
+#import os
 #from dotenv import find_dotenv, load_dotenv
+import boto3
+from botocore.exceptions import ClientError
 
 #dotenv_path = find_dotenv()
 #load_dotenv(dotenv_path)
 #API_KEY = os.getenv("API_KEY")
+
+def get_api_key():
+    secret_name = "Weather-API"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response["SecretString"]
+    data = json.loads(secret)
+    return data["API_KEY"]
 
 class WeatherForecast:
     def __init__(self, city):
         self.City = city.title()
 
     def GetForecast(self):
-        API_KEY = ""
+        API_KEY = get_api_key()
         url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={self.City}&aqi=no"
         response = requests.get(url)
         data = response.json()
